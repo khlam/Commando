cb3a29a22bc71d0e7a6e33a65ddec36763a964a9
khlam <4841220+khlam@users.noreply.github.com>
2021-08-08 10:55:52 -0700

    test disable help DM

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -74,30 +74,9 @@ module.exports = class HelpCommand extends Command {
 			const messages = [];
 			try {
 				messages.push(await msg.direct(stripIndents`
-					${oneLine`
-						To run a command in ${msg.guild ? msg.guild.name : 'any server'},
-						use ${Command.usage('command', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-						For example, ${Command.usage('prefix', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
-					`}
-					To run a command in this DM, simply use ${Command.usage('command', null, null)} with no prefix.
-
-					Use ${this.usage('<command>', null, null)} to view detailed information about a specific command.
-					Use ${this.usage('all', null, null)} to view a list of *all* commands, not just available ones.
-
-					__**${showAll ? 'All commands' : `Available commands in ${msg.guild || 'this DM'}`}**__
-
-					${groups.filter(grp => grp.commands.some(cmd => !cmd.hidden && (showAll || cmd.isUsable(msg))))
-						.map(grp => stripIndents`
-							__${grp.name}__
-							${grp.commands.filter(cmd => !cmd.hidden && (showAll || cmd.isUsable(msg)))
-								.map(cmd => `**${cmd.name}:** ${cmd.description}${cmd.nsfw ? ' (NSFW)' : ''}`).join('\n')
-							}
-						`).join('\n\n')
-					}
-				`, { split: true }));
-				if(msg.channel.type !== 'dm') messages.push(await msg.reply('Sent you a DM with information.'));
+					${oneLine`help DM disabled.`}`, { split: true }));
 			} catch(err) {
-				messages.push(await msg.reply('Unable to send you the help DM. You probably have DMs disabled.'));
+				console.log("error with disabled DMs")
 			}
 			return messages;
 		}
